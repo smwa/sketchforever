1ebef2068c437e5a392ee5b0fab89519aed1733c
Michael Smith <michael.smith.ok@gmail.com>
2023-06-02 09:25:24 -0500

    Add fake fetch handler

--- service-worker.js
+++ service-worker.js
@@ -5,3 +5,7 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   console.log('Service worker activated');
 });
+
+self.addEventListener('fetch', event => {
+  console.log('Someday we\'ll enable offline access');
+});
